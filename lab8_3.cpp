93b5b5ea93f72ba512e38df91ad76bb1bf8565bc
jirayu123za <jirayu004za@gmail.com>
2021-12-03 15:37:36 +0700

    finish_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,18 +2,14 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    if(x == 65){
+        return x = 'Z';
+    }
+    if(x >= 65 && x <= 90){
+        x = x-1;
+    }
+    else{
+        return '0';
+    }
+    return x;
 }
